7456233b9079ce84dc40718de230e53f936be9bd
Sandhya Ratan <M1042983@mindtree.com>
2020-06-15 22:45:19 +0530

    Added Auth

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IAuthRepository authRepository,IConfiguration configuration)
+        {
+            _authRepository = authRepository;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto user)
+        {
+            //can remove modelstate and [frombody] if ApiContrller is user
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            user.UserName = user.UserName.ToLower();
+            if(await _authRepository.UserExist(user.UserName))
+            {
+                return BadRequest("User exist");
+            }
+
+            var userToCreate = new User
+            {
+                UserName = user.UserName
+            };
+
+            var createdUser = await _authRepository.Register(userToCreate, user.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _authRepository.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+            var claims = new[]
+            {
+               new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+               new Claim(ClaimTypes.Name,userFromRepo.UserName)
+           };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject=new ClaimsIdentity(claims),
+                Expires=DateTime.Now.AddDays(1),
+                SigningCredentials=creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new 
+            {
+                token=tokenHandler.WriteToken(token)
+            });
+        }
+    }
+}
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="enter password bet 4 to 8 char")]
+        public string Password { get; set; }
+    }
+}
